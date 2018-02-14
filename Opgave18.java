707a34c3e1dce7039892a1185d29c2530e24afa9
AndreasJepsenNielsen <32238389+AndreasJepsenNielsen@users.noreply.github.com>
2018-02-14 11:14:57 +0100

    Lavet opgave 18 i skole, virker bedre end min egen version

--- Opgave18.java
+++ Opgave18.java
@@ -1,31 +1,53 @@
 import java.util.HashMap;
-import java.util.Iterator;
+import java.util.HashSet;
 import java.util.Map;
-
+import java.util.Set;
 
 public class Opgave18 {
-    public static void main(String[] args) {
-        Map<String,String> map1 = new HashMap<>();
-        map1.put("Anne","54");
-        map1.put("Andreas", "22");
-        map1.put("Kasper", "28");
-        reverse(map1);
 
+    public static void main(String[] args) {
+        Map<String,String> value = new HashMap<>();
+
+        value.put("3", "Marty");
+        value.put("42", "Marty");
+        value.put("31", "Dave");
+        value.put("82", "Sue");
+        value.put("17", "Ed");
+        value.put("29", "Ed");
+        value.put("56", "Ed");
+
+        System.out.println(reverse(value));
 
 
     }
 
-    public static Map reverse(Map<String,String> a)
-    {
-        Map<String,String> map2 = new HashMap<>();
-        Iterator it = a.entrySet().iterator();
 
-        while (it.hasNext()) //
+
+
+    public static Map reverse(Map<String, String> map)
+    {
+        Map<String, Set<String>> value = new HashMap<>();
+
+
+        for (Map.Entry<String, String> m: map.entrySet())
         {
-            Map.Entry P = (Map.Entry)it.next(); //
-            map2.put(P.getValue().toString(),P.getKey().toString()); // sætter værdien sådan at den gamle value er = den nye key, og den gamle key er = den nye value altså byttet om på værdierne
+            if(value.containsKey(m.getValue()))
+            {
+                Set<String> set = value.get(m.getValue());
+
+                set.add(m.getKey());
+
+                value.put(m.getValue(),set);
+
+            }else{
+                Set<String> set = new HashSet<>();
+
+                set.add(m.getKey());
+
+                value.put(m.getValue(),set);
+
+            }
         }
-        System.out.println(map2); // udskriver map for at se om den har byttet om på værdierne
-        return map2;
+        return value;
     }
 }
